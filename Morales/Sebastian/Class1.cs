614295b1664695d23ee3b90fe3b270ad24401717
Sebastian Morales Yabeta <masterrol001@hotmail.com>
2019-11-30 12:12:19 -0400

    2do Commit Correccion de codigo

--- Morales/Sebastian/Class1.cs
+++ Morales/Sebastian/Class1.cs
@@ -2,7 +2,33 @@
 
 namespace Sebastian
 {
-    public class Class1
+    public class Company
     {
+
+        private string _name; private double _saveproject;
+
+        public string Name { get { return _name; } }
+        public double SaveProject { get { return _saveproject; } }
+
+        public Company(string name, double saveproject) //Error: Person en vez de Company y un punto y coma mal ubicado, falta de parentesis 
+        { 
+            _name = name; //Error: Error de typeo
+            _saveproject = saveproject; //Error: Punto y coma mal colocado
+        }
+
+    public void Save(double save)
+    {
+        _saveproject += save; //Error: Punto y coma mal colocado
     }
+
+    public void Spend(double save)
+    {
+        _saveproject += save;  //Error: Punto y coma mal colocado
+    }
+    public void ChangeName(string name)
+    {
+        _name = name;
+    }
+
+}
 }
